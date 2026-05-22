9450d87e9b5e49c7ed62cfd81fc74eeb59d64e44
tofuhaku <tzy8179@gmail.com>
2026-05-22 10:48:52 +0800

    finish week11

--- Week11/403_Frog_Jump.cpp
+++ Week11/403_Frog_Jump.cpp
@@ -4,10 +4,35 @@
 #include <numeric>
 #include <queue>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
+
+class Solution {
+public:
+    bool canCross(vector<int>& stones) {
+        unordered_map<int, unordered_set<int>> dp;
+        for (int stone : stones) {
+            dp[stone] = unordered_set<int>();
+        }
+        dp[stones[0]].insert(0);
+
+        for (int stone : stones) {
+            for (int jump : dp[stone]) {
+                for (int nextJump = jump - 1; nextJump <= jump + 1; ++nextJump) {
+                    if (nextJump > 0 && dp.find(stone + nextJump) != dp.end()) {
+                        dp[stone + nextJump].insert(nextJump);
+                    }
+                }
+            }
+        }
+
+        return !dp[stones.back()].empty();
+    }
+};
 
 int main() {
     Solution sol;
-
+    vector<int> stones = {0,1,3,5,6,8,12,17};
+    cout << sol.canCross(stones) << endl;
 	return 0;
 }
--- Week11/581_Shortest_Unsorted_Continuous_Subarray.cpp
+++ Week11/581_Shortest_Unsorted_Continuous_Subarray.cpp
@@ -6,8 +6,36 @@
 #include <unordered_map>
 using namespace std;
 
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int n = nums.size();
+
+        int max_seen = nums[0];
+        int right = -1;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < max_seen) {
+                right = i;
+            }
+            max_seen = max(max_seen, nums[i]);
+        }
+
+        int min_seen = nums[n - 1];
+        int left = 0;
+        for (int i = n - 1; i > -1; --i) {
+            if (nums[i] > min_seen) {
+                left = i;
+            }
+            min_seen = min(min_seen, nums[i]);
+        }
+
+        return right - left + 1;
+    }
+};
+
 int main() {
     Solution sol;
-
+    vector<int> nums = {2,6,4,8,10,9,15};
+    cout << sol.findUnsortedSubarray(nums) << endl;
 	return 0;
 }
